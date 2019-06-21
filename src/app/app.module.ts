544215d7bb3ce6ee417a3fc92003a9f51bf07ef2
delgado.trueba <delgado.trueba@outlook.es>
2019-06-21 12:44:16 +0200

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
+// <<< Importar Modulos >>>
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material.module';
+
+
+// <<< Importar Componentes >>>
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // Componentes, Pipes y Directivas
   declarations: [
     AppComponent
   ],
+  // Modulos
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    BrowserAnimationsModule,
+    AppMaterialModule,
   ],
+  // Servicos
   providers: [],
   bootstrap: [AppComponent]
 })
