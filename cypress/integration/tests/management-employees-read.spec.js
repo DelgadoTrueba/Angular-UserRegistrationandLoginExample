98c0599288e7e6874d6efefa98070a8238065da8
delgado.trueba <delgado.trueba@outlook.es>
2019-06-25 13:47:45 +0200

    cypress read employee

---
+++ cypress/integration/tests/management-employees-read.spec.js
@@ -0,0 +1,90 @@
+/// <reference types="Cypress" />
+
+describe("Login", () => {
+    
+    beforeEach(() => {
+    
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8080/api/v0/users/token',
+            auth: {
+                'user': 'admin',
+                'pass': 'admin',
+                'sendImmediately': true
+            }
+        }).as("TOKEN");
+
+        cy.get("@TOKEN").then( (response) => {
+            let token = response.headers.authorization.substring(7);
+
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:8080/api/v0/employees',
+                auth:{
+                    'bearer': token,
+                },
+                body: {
+                    id: 0,
+                    firstName: "Jose Manuel",
+                    lastName: "Delgado Trueba",
+                    email: "jd@gmail.com"
+                }
+            })
+            .as("EMPLOYEE")
+
+        });
+
+        cy.visit("/welcome/login");
+
+        cy.get('#userName').type("admin");
+        cy.get('#password').type('admin');
+        cy.get('#submit').click();
+    
+        cy.url().should('include', '/home');
+
+    });
+  
+    it("READ - Employee", () => {
+
+      cy.get('mat-table').find("mat-cell").contains("Jose Manuel").parent().as("field");
+
+      cy.get("@field").contains("visibility").click();
+
+      cy.get('#firstName').invoke('val').then( (inputText) => {
+        expect(inputText).eq('Jose Manuel');
+      });
+      
+      cy.get('#lastName').invoke('val').then( (inputText) => {
+        expect(inputText).eq('Delgado Trueba');
+      });
+      
+      cy.get('#email').invoke('val').then( (inputText) => {
+        expect(inputText).eq('jd@gmail.com');
+      });
+      
+    });
+
+    after( () => {
+       cy.get("@EMPLOYEE").then((response) => {
+            let id = response.body.id;
+
+            cy.get("@TOKEN").then((response) => {
+                let token = response.headers.authorization.substring(7);
+
+                cy.request({
+                    method: 'DELETE',
+                    url: 'http://localhost:8080/api/v0/employees' + "/" + id,
+                    auth:{
+                      'bearer': token,
+                    }
+                }).then( response => {
+                    expect(response.status).to.eq(200)
+                })
+            
+            })
+
+        })
+    })
+
+
+});
